66dcfd2cf7220f73775f20aee4e833d21c693d72
PathOfNoob <info@gavrilgulyaev.ru>
2025-09-30 01:58:40 +0900

    first commit

---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <Nav />
+    <h1>Hello World!</h1>
+  </div>
+</template>
---
+++ app/components/Nav.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </div>
+</template>
